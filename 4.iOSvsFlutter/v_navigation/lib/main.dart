3f0e0b417f15be81f3e2abf56dad8bc5e205c520
Reed <hjphfut@163.com>
2019-02-28 19:03:03 +0800

    路由

---
+++ 4.iOSvsFlutter/v_navigation/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'green_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+      routes: {
+        // 1.map中配置路由
+        '/green': (BuildContext context) => GreenPage()
+      },
+    );
+  }
+}
+/*
+导航到新页面并返回
+  1.起始位置routes中通过Map定义;
+  2.直接创建
+给新页面传值
+从新页面返回数据给上一个页面
+
+ */
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          child: Text('跳转到新页面'),
+          onPressed: () {
+            // 通过路由名字跳转新路由
+            // Navigator.of(context).pushNamed('/green');
+            // 直接创建路由并跳转
+            Navigator.push(context, MaterialPageRoute(builder: (context) => GreenPage()));
+          },
+        ),
+      ),
+    );
+  }
+}
