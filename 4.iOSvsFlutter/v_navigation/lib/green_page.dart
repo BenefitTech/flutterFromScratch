c684ff5f2c886646a34cc7e48de41103a366edf7
Reed <hjphfut@163.com>
2019-03-01 10:30:53 +0800

    完善路由

--- 4.iOSvsFlutter/v_navigation/lib/green_page.dart
+++ 4.iOSvsFlutter/v_navigation/lib/green_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class GreenPage extends StatelessWidget {
+  GreenPage({Key key ,@required this.parameter}):super(key:key);
+  final String parameter;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,11 +10,20 @@ class GreenPage extends StatelessWidget {
         color: Colors.green,
         child: Center(
           child: RaisedButton(
-            child: Text('返回'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('返回'),
+                Text('$parameter')
+              ],
+            ),
             onPressed: () {
+
               print('返回');
+              // 5.pop回去
               // Navigator.of(context).pop('返回参数');
-              Navigator.pop(context, 'returnsth');
+              Navigator.pop(context, '返回参数');
             },
           ),
         ),
--- 4.iOSvsFlutter/v_navigation/lib/main.dart
+++ 4.iOSvsFlutter/v_navigation/lib/main.dart
@@ -38,23 +38,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          child: Text('跳转到新页面'),
-          onPressed: () {
-            // 通过路由名字跳转新路由
-            // Navigator.of(context).pushNamed('/green');
-            // 直接创建路由并跳转
-            _navigateToGreenPage(context);
-          },
-        ),
-      ),
+
+      // 这里需要使用builder,传入的context才能被后面的showSnackBar使用,否则报错:Unhandled Exception: Scaffold.of() called with a context that does not contain a Scaffold.
+      body: Builder(builder: (BuildContext context) {
+        return Center(
+          child: RaisedButton(
+            child: Text('跳转到新页面'),
+            onPressed: () {
+              // 通过路由名字跳转新路由
+              // Navigator.of(context).pushNamed('/green');
+              // 2.直接创建路由并跳转
+              _navigateToGreenPage(context);
+            },
+          ),
+        );
+      }),
     );
   }
 
-  void _navigateToGreenPage(BuildContext context) async {
+  _navigateToGreenPage(BuildContext context) async {
+
+    // 3.新页面传值
+    // 4.接收新页面返回值
     final result = await Navigator.push(
-        context, MaterialPageRoute(builder: (context) => GreenPage()));
+        context, MaterialPageRoute(builder: (context) => GreenPage(parameter: '传入参数')));
     Scaffold.of(context).showSnackBar(SnackBar(
       content: Text('$result'),
     ));
