e70cabae0d167e8929af04f6973bbf506854b2e5
Reed <hjphfut@163.com>
2019-03-05 16:13:35 +0800

    构建纯dart package

---
+++ 5.PackagesDeveloping/i_pure_dart_package/lib/i_pure_dart_package.dart
@@ -0,0 +1,20 @@
+library i_pure_dart_package;
+
+/*
+Developing Dart packages
+Step 1: 开发Dart包
+要创建Dart包，请使用--template=package 来执行 flutter create
+  flutter create --template=package hello
+这将在hello/文件夹下创建一个具有以下专用内容的package工程：
+
+  lib/hello.dart:
+    Package的Dart代码
+  test/hello_test.dart:
+    Package的单元测试代码.
+ */
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
