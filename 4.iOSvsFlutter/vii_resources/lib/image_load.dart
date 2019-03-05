40350e95cde309517780fe8c49847e1746971a3e
Eric <hjphfut@163.com>
2019-03-05 23:45:34 +0800

    补充渐进图片和缓冲图片

--- 4.iOSvsFlutter/vii_resources/lib/image_load.dart
+++ 4.iOSvsFlutter/vii_resources/lib/image_load.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './const/Strings.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 /*
 对于图片，Flutter 像 iOS 一样，遵循了一个简单的基于像素密度的格式。Image assets 可能是 1.0x 2.0x 3.0x 或是其他的任何倍数。这些所谓的 devicePixelRatio 传达了物理像素到单个逻辑像素的比率。
@@ -18,6 +20,17 @@ class ImageLoadView extends StatelessWidget {
           Image.asset(Strings.shareWechat),
           Image(
             image: AssetImage(Strings.shareQQ),
+          ),
+          FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image:
+                'http://e0.ifengimg.com/05/2018/1116/116C80163001B0C28C10555C5CB4DFC7488552B7_size125_w1080_h812.jpeg',
+          ),
+          CachedNetworkImage(
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+            imageUrl:
+                'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=321807542,3031820178&fm=26&gp=0.jpg',
           )
         ],
       ),
