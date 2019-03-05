4cff5f62ba2974439b95e6db94eef954178704a0
Reed <hjphfut@163.com>
2019-03-05 16:27:52 +0800

    try 单元测试

--- 5.PackagesDeveloping/i_pure_dart_package/test/i_pure_dart_package_test.dart
+++ 5.PackagesDeveloping/i_pure_dart_package/test/i_pure_dart_package_test.dart
@@ -3,11 +3,28 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:i_pure_dart_package/i_pure_dart_package.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+//   test('adds one to input values', () {
+//     final calculator = Calculator();
+//     expect(calculator.addOne(2), 3);
+//     expect(calculator.addOne(-7), -6);
+//     expect(calculator.addOne(0), 1);
+//     expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+//     print('test end');
+//   });
+  // 单元测试:
+
+  group('Calculate test--', () {
+    test('adds one to input values', () {
+      final calculator = Calculator();
+      expect(calculator.addOne(2), 3);
+      expect(calculator.addOne(-7), -6);
+      expect(calculator.addOne(0), 1);
+      expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    });
+
+    test('exception test', (){
+      final cal =Calculator();
+      expect(cal.addOne(null), 1);
+    });
   });
 }
